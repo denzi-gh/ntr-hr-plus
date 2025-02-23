c4b302b61c7fa45b624e0e5b1a84c0cda19f5e7d
JS Deck <jsdeckerido@gmail.com>
2025-02-23 00:53:52 -0400

    More tolerance for inaccurate bandwidth detection

--- source/nwm_misc/ikcp.c
+++ source/nwm_misc/ikcp.c
@@ -470,7 +470,7 @@ static int ikcp_input_handle_nack(ikcpcb *kcp, struct IQUEUEHEAD *queue, int g, 
 #define KCP_CONGC_DEC_HTHRES (4)
 #define KCP_CONGC_DEC_MINF (8)
 #define KCP_CONGC_INC_THRES (32)
-#define KCP_CONGC_INC_MAXF (8)
+#define KCP_CONGC_INC_MAXF (4)
 #define KCP_CONGC_INC_RATEF (64)
 
 static void ikcp_set_qos(u32 qos)
@@ -508,11 +508,12 @@ static int ikcp_input_congc(ikcpcb *kcp)
 
 		avg_count_total = kcp->congc.avg_ack_count + kcp->congc.avg_nack_count;
 #define avg_nack_iratio (avg_count_total / kcp->congc.avg_nack_count)
+#define avg_qos (((IUINT64)kcp->congc.avg_ack_count * PACKET_SIZE * SYSCLOCK_ARM11 / kcp->congc.avg_dur) >> 16)
 
 		if (kcp->congc.last_nack_count && kcp->congc.avg_nack_count && avg_nack_iratio < KCP_CONGC_DEC_THRES) {
 			IUINT32 qos;
 			if (avg_nack_iratio < KCP_CONGC_DEC_HTHRES) {
-				qos = ((IUINT64)kcp->congc.avg_ack_count * PACKET_SIZE * SYSCLOCK_ARM11 / kcp->congc.avg_dur) >> 16;
+				qos = avg_qos;
 				kcp->congc.avg_nack_count = 0;
 				kcp->congc.avg_dur = (IUINT64)kcp->congc.avg_dur * kcp->congc.avg_ack_count / avg_count_total;
 			} else {
@@ -531,7 +532,7 @@ static int ikcp_input_congc(ikcpcb *kcp)
 			kcp->congc.qos = qos;
 			ikcp_set_qos(qos);
 		} else if (kcp->congc.avg_nack_count == 0 || avg_nack_iratio >= KCP_CONGC_INC_THRES) {
-			IUINT32 qos_thres = (((IUINT64)kcp->congc.avg_ack_count * PACKET_SIZE * SYSCLOCK_ARM11 / kcp->congc.avg_dur) >> 16) + kcp->qos / KCP_CONGC_INC_MAXF;
+			IUINT32 qos_thres = avg_qos + kcp->qos / KCP_CONGC_INC_MAXF;
 			IUINT32 qos = kcp->congc.qos + kcp->qos / KCP_CONGC_INC_RATEF;
 			if (qos <= qos_thres) {
 				if (qos > kcp->qos) {
@@ -541,6 +542,7 @@ static int ikcp_input_congc(ikcpcb *kcp)
 				ikcp_set_qos(qos);
 			}
 		}
+		// nsDbgPrint("avg qos: %"PRIu32", current qos: %"PRIu32"\n", (IUINT32)avg_qos, kcp->congc.qos);
 
 		kcp->congc.last_ack_count = kcp->congc.last_nack_count = 0;
 	}
