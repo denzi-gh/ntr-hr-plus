cd36ed37a377c2d5c88016ce7305d0d5b5eb8bca
JS Deck <jsdeckerido@gmail.com>
2025-08-19 03:35:10 -0300

    clarify wip prep

--- source/menu/rp_menu.c
+++ source/menu/rp_menu.c
@@ -306,7 +306,7 @@ static const char *getDownsampleDesc(int i) {
 	switch (i) {
 		default:
 		case RP_DOWNSAMPLE_NONE:
-			return "Choose downsample method to improve\nframerate at the cost of quality.";
+			return "Choose downsample method to improve\nframerate at the cost of quality.\nRequires Reliable Stream.";
 		case RP_DOWNSAMPLE_CHECKER:
 			return "Checkerboard: Alternate checkerboard\npattern every other frame.";
 		case RP_DOWNSAMPLE_EVEN_ODD:
